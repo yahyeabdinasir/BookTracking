20fba1d06a365a57ee502c48485710bc50209e5f
yahyeabdinasir <yahyeabdinasirfarah@gmail.com>
2026-03-21 19:25:03 +0300

    reav : refactoring the griedview widget

---
+++ lib/screen/GridViewWidgetFile.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:read_tracking/models/books.dart';
+import 'package:read_tracking/utils/bookargumentsddetails.dart';
+class GridViewWidget extends StatelessWidget {
+  const GridViewWidget({super.key, required this.data});
+
+  final List<Books> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.builder(
+        itemCount: data.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.6,
+          // mainAxisSpacing: 2
+        ),
+        itemBuilder: (context, index) {
+          // print("this is the context ${context}");
+          Books book = data[index];
+          print("this is the books as below index , ${book}");
+          // final imageUrl = book.thumbmail.trim();
+
+          return Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surfaceBright,
+              borderRadius: BorderRadiusDirectional.circular(10),
+            ),
+
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    //  navigate to the details screen throght passing the route of the screen
+                    //   Navigator.push(context, MaterialPageRoute(
+                    //       builder: (context) => DetailsScreen()
+                    //   ),);
+
+                    // Navigator.push(context, the name of the route from the main dart )
+                    Navigator.pushNamed(
+                      context,
+                      '/detailScreen',
+                      arguments: BookDetailsArguments(bookItems: book),
+                    );
+
+                    print("hello there this si the gesture  ");
+                  },
+
+                  child: Padding(
+                    padding: const EdgeInsetsGeometry.all(15),
+                    child: Card(
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Image.network(book.thumbmail, fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  book.title,
+                  style: Theme.of(context).textTheme.titleSmall,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  book.authors.isNotEmpty
+                      ? book.authors.join(" & ")
+                      : "No author",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  //  so this is the properties that we use to make clip and fit the text when there is the overflow of the text
+                  //  so that is how we can use them  so the elipse makes shorter the larget text along with the ..
+                  //  while the clip attribute also make them fit  the entire  text without overflow
+                  //  and also the visible and fade attribute  they put the top of it each text without the overflow
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:read_tracking/models/books.dart';
+import 'package:read_tracking/screen/GridViewWidgetFile.dart';
 import 'package:read_tracking/screen/bookDetails.dart';
 import 'package:read_tracking/service/googleApi.dart';
 import 'package:read_tracking/utils/bookargumentsddetails.dart';
@@ -28,9 +29,6 @@ class _MyHomeState extends State<MyHome> {
   // initialisin the controller  text for the editable text  of the user
   TextEditingController _TextContorller = TextEditingController();
 
-
-
-
   void SearchBook(String Query) async {
     try {
       List<Books>? _result = await Network.SearchBooks(Query);
@@ -48,47 +46,40 @@ class _MyHomeState extends State<MyHome> {
     }
   }
 
-
-
   @override
   void initState() {
     super.initState();
 
     _TextContorller.addListener(() {
       print('this is going user type ${_TextContorller.text}');
-
     });
   }
-@override
+
+  @override
   void dispose() {
     // TODO: implement dispose
-  print("❌ Controller waa la dispose gareeyay");
-  _TextContorller.dispose();
-  super.dispose();
-
+    print("❌ Controller waa la dispose gareeyay");
+    _TextContorller.dispose();
+    super.dispose();
   }
-  
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
-
         children: [
           Padding(
             padding: const EdgeInsets.all(10.0),
 
             child: TextField(
-              
               // this used to get the text from the user
               controller: _TextContorller,
               // decoration: BoxDecoration(
               //   borderRadius: BorderRadius.circular(20),
               //   border: Border.all(color: Theme.of(context).primaryColor),
               decoration: InputDecoration(
-                 fillColor: Colors.white,
+                fillColor: Colors.white,
 
                 label: Text("Search for a book"),
                 hintText: "enter a book",
@@ -101,7 +92,7 @@ class _MyHomeState extends State<MyHome> {
                   onPressed: () {
                     // callin the api then passin the user input to the api
                     SearchBook(_TextContorller.text);
-                  }
+                  },
                 ),
               ),
               onSubmitted: (Query) {
@@ -109,96 +100,7 @@ class _MyHomeState extends State<MyHome> {
               },
             ),
           ),
-          Expanded(
-            child: GridView.builder(
-              itemCount: data.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.6,
-                  // mainAxisSpacing: 2
-              ),
-              itemBuilder: (context, index) {
-                // print("this is the context ${context}");
-                Books book = data[index];
-                print("this is the books as below index , ${book}");
-                final imageUrl = book.thumbmail.trim();
-
-                return Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Theme
-                        .of(context)
-                        .colorScheme.surfaceBright,
-                    borderRadius: BorderRadiusDirectional.circular(10),
-                  ),
-
-                  child: Column(
-
-                    children: <Widget>[
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                          //  navigate to the details screen throght passing the route of the screen
-                          //   Navigator.push(context, MaterialPageRoute(
-                          //       builder: (context) => DetailsScreen()
-                          //   ),);
-
-                            // Navigator.push(context, the name of the route from the main dart )
-                            Navigator.pushNamed(context, '/detailScreen' , arguments: BookDetailsArguments(bookItems: book));
-
-
-
-
-                          });
-                          print("hello there this si the gesture  ");
-                  },
-
-                        child: Padding(
-                          padding: const EdgeInsetsGeometry.all(15),
-                          child: Card(
-                            child: SizedBox(
-                              height: 120,
-                              width: double.infinity,
-                              child: Image.network(imageUrl,
-                                fit: BoxFit.fill,
-                                loadingBuilder: (context, child, progress) {
-                                  if (progress == null) return child;
-                                  return const Center(
-                                    child: CircularProgressIndicator(strokeWidth: 2),
-                                  );
-                                },
-                                errorBuilder: (context, error, stackTrace) {
-                                  return const Center(
-                                    child: Icon(Icons.broken_image, size: 40),
-                                  );
-                                },
-                              )
-                            ),
-                          ),
-                        ),
-                      ),
-                      Text(book.title,
-                      style:  Theme.of(context).textTheme.titleSmall,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(book.authors.isNotEmpty
-                          ? book.authors.join(" & ")
-                          : "No author",style: Theme.of(context).textTheme.titleMedium,
-                        //  so this is the properties that we use to make clip and fit the text when there is the overflow of the text
-                        //  so that is how we can use them  so the elipse makes shorter the larget text along with the ..
-                        //  while the clip attribute also make them fit  the entire  text without overflow
-                        //  and also the visible and fade attribute  they put the top of it each text without the overflow
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-
-
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
+          GridViewWidget(data: data),
         ],
       ),
     );
