01c0910a05308c71c67c6c2212afa6c5f6b7f997
yahyeabdinasir <yahyeabdinasirfarah@gmail.com>
2026-03-28 11:11:43 +0300

    feat  :  designing the layout of the save file

--- lib/screen/safeFile.dart
+++ lib/screen/safeFile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:read_tracking/db/BookDb.dart';
 import 'package:read_tracking/models/books.dart';
 import 'package:read_tracking/service/googleApi.dart';
@@ -22,7 +23,6 @@ class _SafefileState extends State<Safefile> {
     // final args = ModalRoute.of(context)!.settings.arguments as  BookDetailsArguments;
     //   args.bookItems;
 
-
     // void SearchBook(book)async{
     //  List<Books>? result =  await network.SearchBooks(book);
     //  print('this is the result from the save file ${result}');
@@ -35,26 +35,55 @@ class _SafefileState extends State<Safefile> {
     // }
     return Scaffold(
       body: FutureBuilder(
-          future: BookDataBase.instance.FetchBooks(),
-          builder: (context, snapchat) =>
-          snapchat.hasData ?
-          ListView.builder(
-            itemCount: snapchat.data!.length,
-            itemBuilder: (context, index) {
-              print("this is the curent index from the save file  $index");
-              Books books = snapchat.data![index];
-              print("Fetched books: ${snapchat.data}");
-              return Card(
-                child:listTtitle
+        future: BookDataBase.instance.FetchBooks(),
+        builder: (context, snapchat) => snapchat.hasData
+            ? ListView.builder(
+                itemCount: snapchat.data!.length,
+                itemBuilder: (context, index) {
+                  print("this is the curent index from the save file  $index");
+                  Books books = snapchat.data![index];
+                  print("Fetched books: ${snapchat.data}");
 
-                title: Text(books.title),
-              );
-            }  )
+                  return Card(
+                    color: Colors.grey,
+                    shape: Border.all(color: Colors.grey.shade300, width: 8.0),
 
-            :const Center(child: CircularProgressIndicator())),
+                    child: ListTile(
+                      title: Text(books.title, style: TextStyle(fontSize: 20)),
+                      trailing: Icon(Icons.delete),
+                      leading: Image.network(
+                        width: 60,
 
-);
+                        books.imageLinks['thumbnail']!,
+                        fit: BoxFit.fill,
+                      ),
+                      subtitle: Column(
+                        children: [
+                          Center(
+                            child: Text(
+                              books.authors.join(" &"),
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+
+                          ElevatedButton.icon(
+                            onPressed: () async {
+                              print("hello yahye ");
+                            },
+                            icon: Icon(Icons.favorite),
+                            label: Text(" add favorite"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              )
+            : const Center(child: CircularProgressIndicator()),
+      ),
+    );
   }
 }
-
-
